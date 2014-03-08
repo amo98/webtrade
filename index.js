126f11a8ac259a6a6748df8a3ed6a832197cc3b3
amo <amo98@sina.com>
2014-03-08 08:54:01 +0800

    0.01

---
+++ index.js
@@ -0,0 +1,121 @@
+
+
+var reg;
+var method = { "method": ["getInfo", "TransHistory", "TradeHistory", "OrderList", "Trade", "CancelOrder"] };
+var param = {
+    "getInfo": "",
+    "TransHistory": { "from": "", "count": "", "from_id": "", "end_id": "", "order": "", "since": "", "end": "60", "pair": "100" },
+    "OrderList": { "from": "", "count": "", "from_id": "", "end_id": "", "order": "", "since": "", "end": "", "pair": "", "active": "" },
+    "Trade": { "pair": "", "type": "", "rate": "", "amount": "" },
+    "CancelOrder": { "order_id": "" },
+    "method": "",
+    "nonce": 0
+};
+var result = {
+    "getInfo": "",
+    "TransHistory": "",
+    "OrderList": "",
+    "Trade": "",
+    "CancelOrder": "",
+    "ticker": { "last": 0, "buy": 0, "sell": 0 },
+    "depth": { "asks": [[]], "bids": [[]] }
+};
+
+var chosenEntry = null;
+
+function sign(secret, message) {
+    return CryptoJS.HmacSHA512(message, secret);
+}
+
+function btcReq(reqParam, reqUrl, callback) {
+
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        callback(JSON.parse(xhr.response));
+    };
+    xhr.open("POST", reqUrl, true);
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhr.setRequestHeader("key", reg.user[0].key);
+    xhr.setRequestHeader("sign", sign(reg.user[0].sec, reqParam));
+    xhr.send(reqParam);
+};
+function makeReqParam() {
+    var i;
+    var str;
+    str = "method=" + param.method + "&nonce=" + param.nonce;
+    for (i in param[param.method]) {
+        if (param[param.method][i])
+            (str) ? (str = str + "&" + i + "=" + param[param.method][i]) : (str = i + "=" + param[param.method][i]);
+    } 
+    return str
+}
+function btcApi(method, callback) {
+    param.method = method;
+
+    var callbackReq = function (respon) {
+        param.nonce++;
+        result[param.method] = respon;
+        callback(method);
+    };
+    var reqParam = makeReqParam(param);
+    console.log("str=" + reqParam);
+    btcReq(reqParam, reg.btcApiUrl, callbackReq);
+
+};
+
+// for files, read the text content into the textarea
+
+
+var ticker = document.getElementById("ticker");
+var input = document.getElementById("input");
+var quit= document.getElementById("quit");
+
+ticker.onclick = function () {
+    btcApi("getInfo", function (method) { console.log(result[method]) });
+};
+
+input.addEventListener('click', function (e) {
+    var accepts = [{
+        mimeTypes: ['text/*'],
+        extensions: ['json']
+    }];
+    chrome.fileSystem.chooseEntry({ type: 'openFile', accepts: accepts }, function (theEntry) {
+        if (!theEntry) {
+            output.textContent = 'No file selected.';
+            return;
+        }
+        // use local storage to retain access to this file
+        chrome.storage.local.set({ 'chosenFile': chrome.fileSystem.retainEntry(theEntry) });
+        chosenEntry = theEntry;
+        chosenEntry.file(function (file) {
+            var reader = new FileReader();
+            reader.onloadend = function () {
+                console.log(this.result);
+                reg = JSON.parse(this.result);
+                param.nonce = reg.user[0].nonce;//reg.user[0].nonce;
+                console.log("nonce="+param.nonce);
+            };
+            reader.readAsText(file);
+        });
+    });
+
+    
+    console.log("click nonce" + param.nonce);
+});
+
+
+quit.addEventListener('click', function (e) {
+    reg.user[0].nonce = param.nonce;
+    console.log("quit nonce" + param.nonce);
+    chrome.fileSystem.getWritableEntry(chosenEntry, function (writableFileEntry) {
+        writableFileEntry.createWriter(function (writer) {
+            var blob = new Blob([JSON.stringify(reg)], { type: "text/plain" });
+            writer.onwriteend = function (e) { writer.write(blob); writer.onwriteend = function (e) { console.log("Writer end!") }};
+            writer.truncate(0);
+            
+            
+        }, function (e) { console.log(e) });
+    });
+
+});
+
