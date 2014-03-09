cf00fcc518a0493d2f47768ddbdd1bf905edaf02
amo <amo98@sina.com>
2014-03-09 18:36:07 +0800

    Revert "0.01"
    This reverts commit 51b8d625465af888a5cb9c95ad7b1e4ffdcaef3c.

--- index.js
+++ index.js
@@ -2,31 +2,6 @@
 
 var reg;
 var method = { "method": ["getInfo", "TransHistory", "TradeHistory", "OrderList", "Trade", "CancelOrder"] };
-var users = {
-    user:[
-    {
-        param: {
-            "getInfo": "",
-            "TransHistory": { "from": "", "count": "", "from_id": "", "end_id": "", "order": "", "since": "", "end": "60", "pair": "100" },
-            "OrderList": { "from": "", "count": "", "from_id": "", "end_id": "", "order": "", "since": "", "end": "", "pair": "", "active": "" },
-            "Trade": { "pair": "", "type": "", "rate": "", "amount": "" },
-            "CancelOrder": { "order_id": "" },
-            "method": "",
-            "nonce": 0
-        },
-        result: {
-            "getInfo": "",
-            "TransHistory": "",
-            "OrderList": "",
-            "Trade": "",
-            "CancelOrder": "",
-            "ticker": { "last": 0, "buy": 0, "sell": 0 },
-            "depth": { "asks": [[]], "bids": [[]] }
-        }
-    }
-
-]
-};
 var param = {
     "getInfo": "",
     "TransHistory": { "from": "", "count": "", "from_id": "", "end_id": "", "order": "", "since": "", "end": "60", "pair": "100" },
@@ -47,9 +22,6 @@ var result = {
 };
 
 var chosenEntry = null;
-var chosenUser = 0;
-var chosenMethod = null;
-var chosenTrade = "btc/ltc";
 
 function sign(secret, message) {
     return CryptoJS.HmacSHA512(message, secret);
@@ -90,24 +62,18 @@ function btcApi(method, callback) {
     btcReq(reqParam, reg.btcApiUrl, callbackReq);
 
 };
-function load() {
 
-
-}
 // for files, read the text content into the textarea
 
 
 var ticker = document.getElementById("ticker");
 var input = document.getElementById("input");
 var quit= document.getElementById("quit");
-var output = document.getElementById("output");
+
 ticker.onclick = function () {
     btcApi("getInfo", function (method) { console.log(result[method]) });
 };
 
-function log(text) {
-    output.textContent = text + "\r\n" + output.textContent;
-}
 input.addEventListener('click', function (e) {
     var accepts = [{
         mimeTypes: ['text/*'],
@@ -115,7 +81,7 @@ input.addEventListener('click', function (e) {
     }];
     chrome.fileSystem.chooseEntry({ type: 'openFile', accepts: accepts }, function (theEntry) {
         if (!theEntry) {
-            log( 'No file selected.');
+            output.textContent = 'No file selected.';
             return;
         }
         // use local storage to retain access to this file
@@ -124,15 +90,17 @@ input.addEventListener('click', function (e) {
         chosenEntry.file(function (file) {
             var reader = new FileReader();
             reader.onloadend = function () {
-                log( this.result );
+                console.log(this.result);
                 reg = JSON.parse(this.result);
                 param.nonce = reg.user[0].nonce;//reg.user[0].nonce;
-                log( "nonce=" + param.nonce );
+                console.log("nonce="+param.nonce);
             };
             reader.readAsText(file);
         });
-    });    
-    log("click nonce" + param.nonce);
+    });
+
+    
+    console.log("click nonce" + param.nonce);
 });
 
 
@@ -151,6 +119,3 @@ quit.addEventListener('click', function (e) {
 
 });
 
-function test(){
-    
-}
