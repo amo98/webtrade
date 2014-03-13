27068e2ca2db9bcbb10ecb6f6f04429f7d62d6ae
amo <amo98@sina.com>
2014-03-13 22:43:43 +0800

    0.02
    0.02

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var reg;
 var method = { "method": ["getInfo", "TransHistory", "TradeHistory", "OrderList", "Trade", "CancelOrder"] };
+var TRADE = { "trade": ["ltc_btc", "nmc_btc", "ppc_btc", "btc_usd", "ltc_usd", "nmc_usd", "ppc_usd"]};
 var users = {
     user:[
     {
@@ -49,31 +50,41 @@ var result = {
 var chosenEntry = null;
 var chosenUser = 0;
 var chosenMethod = null;
-var chosenTrade = "btc/ltc";
+var chosenTrade = "ltc_btc";
 
 function sign(secret, message) {
     return CryptoJS.HmacSHA512(message, secret);
 }
 
 function btcReq(reqParam, reqUrl, callback) {
-
+    log(reqUrl);
     var xhr = new XMLHttpRequest();
     xhr.onload = function () {
+        log(xhr.response);
         callback(JSON.parse(xhr.response));
     };
-    xhr.open("POST", reqUrl, true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("key", reg.user[0].key);
-    xhr.setRequestHeader("sign", sign(reg.user[0].sec, reqParam));
-    xhr.send(reqParam);
+
+    if (reqParam != "") {
+        log("reqParam=" + reqParam);
+        xhr.open("POST", reqUrl, true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.setRequestHeader("key", reg.user[0].key);
+        xhr.setRequestHeader("sign", sign(reg.user[0].sec, reqParam));
+
+        xhr.send(reqParam);
+    } else {
+        log("GET " + reqUrl);
+        xhr.open("GET", reqUrl, true);
+        xhr.send();
+    }
 };
 function makeReqParam() {
     var i;
     var str;
-    str = "method=" + param.method + "&nonce=" + param.nonce;
-    for (i in param[param.method]) {
-        if (param[param.method][i])
-            (str) ? (str = str + "&" + i + "=" + param[param.method][i]) : (str = i + "=" + param[param.method][i]);
+    str = "method=" + param.method + "&nonce=" + users.user[chosenUser].param.nonce;
+    for (i in users.user[chosenUser].param[param.method]) {
+        if (users.user[chosenUser].param[param.method][i])
+            (str) ? (str = str + "&" + i + "=" + users.user[chosenUser].param[param.method][i]) : (str = i + "=" + users.user[chosenUser].param[param.method][i]);
     } 
     return str
 }
@@ -81,8 +92,8 @@ function btcApi(method, callback) {
     param.method = method;
 
     var callbackReq = function (respon) {
-        param.nonce++;
-        result[param.method] = respon;
+        users.user[chosenUser].param.nonce++;
+        users.user[chosenUser].result[param.method] = respon;
         callback(method);
     };
     var reqParam = makeReqParam(param);
@@ -90,25 +101,38 @@ function btcApi(method, callback) {
     btcReq(reqParam, reg.btcApiUrl, callbackReq);
 
 };
-function load() {
+function load(file) {
 
 
 }
-// for files, read the text content into the textarea
-
-
-var ticker = document.getElementById("ticker");
-var input = document.getElementById("input");
-var quit= document.getElementById("quit");
-var output = document.getElementById("output");
-ticker.onclick = function () {
-    btcApi("getInfo", function (method) { console.log(result[method]) });
-};
-
 function log(text) {
     output.textContent = text + "\r\n" + output.textContent;
 }
-input.addEventListener('click', function (e) {
+// for files, read the text content into the textarea
+
+var trader = document.getElementById("Trader");
+var ticker = document.getElementById("Ticker");
+var loader = document.getElementById("Loader");
+var saver = document.getElementById("Saver");
+var pricer = document.getElementById("Pricer");
+var output = document.getElementById("output");
+var bors = document.getElementById("buyorsell");
+var sltuser = document.getElementById("User");
+var nc = document.getElementById("nonce");
+nc.onclick = function () { users.user[chosenUser].param.nonce++ };
+
+sltuser.onchange=function (e) {
+    chosenUser = sltuser.value;
+    log("chosenUser=" + chosenUser);
+};
+
+ticker.onclick = function () {
+    //btcApi("getInfo", function (method) { console.log(result[method]) });
+
+};
+
+
+loader.addEventListener('click', function (e) {
     var accepts = [{
         mimeTypes: ['text/*'],
         extensions: ['json']
@@ -124,21 +148,32 @@ input.addEventListener('click', function (e) {
         chosenEntry.file(function (file) {
             var reader = new FileReader();
             reader.onloadend = function () {
-                log( this.result );
+                log(this.result);
                 reg = JSON.parse(this.result);
-                param.nonce = reg.user[0].nonce;//reg.user[0].nonce;
-                log( "nonce=" + param.nonce );
+                users.user[chosenUser].param.nonce = (reg.user[0].nonce);//reg.user[0].nonce;
+                trader.innerHTML = "";
+                for (i in TRADE.trade) {
+                    trader.innerHTML += "<option value='" + TRADE.trade[i] + "'>" + TRADE.trade[i] + "</option>";
+                }
+                btcReq("", reg.url + reg.chosenTrade + "/ticker", function (resu) { pricer.value = resu.ticker.last; });
+                sltuser.innerHTML = "";
+                for (i in reg.user) {
+                    sltuser.innerHTML += "<option value='" + reg.user[i].id + "'>" + reg.user[i].name + "</option>";
+                }
+                log("nonce=" + users.user[chosenUser].param.nonce);
+                btcApi("getInfo", function (method) { log(JSON.stringify(users.user[chosenUser].result[method])) });
             };
             reader.readAsText(file);
+
         });
     });    
-    log("click nonce" + param.nonce);
+    log("click nonce" + users.user[chosenUser].param.nonce);
 });
 
 
-quit.addEventListener('click', function (e) {
-    reg.user[0].nonce = param.nonce;
-    console.log("quit nonce" + param.nonce);
+saver.addEventListener('click', function (e) {
+    reg.user[chosenUser].nonce = users.user[chosenUser].param.nonce;
+    console.log("saver nonce" + users.user[chosenUser].param.nonce);
     chrome.fileSystem.getWritableEntry(chosenEntry, function (writableFileEntry) {
         writableFileEntry.createWriter(function (writer) {
             var blob = new Blob([JSON.stringify(reg)], { type: "text/plain" });
